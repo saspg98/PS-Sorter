483b78d994ae6c73fb7c83e95e91d3029c77704b
Saurav Gupta <sauravgupta97@gmail.com>
2019-04-07 21:02:34 +0530

    Update ps-sorter.py

--- ps-sorter.py
+++ ps-sorter.py
@@ -30,7 +30,7 @@ def Clicker():
 
 email=sys.argv[1]
 password=sys.argv[2]
-driver = webdriver.Chrome(executable_path='C:\\chromedriver.exe')
+driver = webdriver.Firefox(executable_path='C:\\chromedriver.exe')
 driver.get('http://psd.bits-pilani.ac.in/Login.aspx')
 time.sleep(1)
 driver.find_element_by_xpath("""//*[@id="TxtEmail"]""").send_keys(email)
